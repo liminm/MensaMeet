0ab8093b768a1bc158e1e6bacb6b007cf8d69534
Todor Moskov <todor.moskov96@gmail.com>
2019-05-28 16:31:39 +0200

    Account functionality created,#5 + fixed structure

--- mensameet_project/urls.py
+++ mensameet_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views as user_views
+from mensameet import views as mensameet_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+    path('home/', mensameet_views.home, name='mensameet-home'),
+	path('register/', user_views.register, name='mensameet-register'),
+	path('', include('mensameet.urls')),
 ]
