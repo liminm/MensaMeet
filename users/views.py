30fbd3341b63238df10d438a281ac78fb0af2425
Todor Moskov <todor.moskov96@gmail.com>
2019-06-13 15:02:47 +0200

    Correct MeetupUpdate topic processing

--- users/views.py
+++ users/views.py
@@ -118,7 +118,7 @@ class MeetupUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 		
 	def get_context_data(self, **kwargs):
 		context = super(MeetupUpdateView, self).get_context_data(**kwargs)
-		form = MeetupUpdateForm(self.request.POST or None)
+		form = MeetupUpdateForm(instance=self.get_object())
 		context["form"] = form
 		return context
 
