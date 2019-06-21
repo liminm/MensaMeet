f64f3c4764d71a76f8a4f3d2f7e3774c27f0c87c
Vesela Stefanova <vesela.al.stefanova@gmail.com>
2019-06-21 21:51:29 +0200

    update date filter

--- mensameet/static/mensameet/main.js
+++ mensameet/static/mensameet/main.js
@@ -1,110 +1,102 @@
-	var coll = document.getElementsByClassName("collaps");
-    var i;
+var coll = document.getElementsByClassName("collaps");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    var content = document.getElementById("allFilter");
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        var content = document.getElementById("allFilter");
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
+var membersFilter, topicsFilter, topicsFilterArr, mensaFilter, dateFilter, fullFilter;
+function updateFilters() {
+  $('.task-list-row').hide().filter(function() {
+    var
+      self = $(this),
+      result = true;
+
+    if (membersFilter && (membersFilter != 'Show All')) {
+      result = result && membersFilter === self.data('members-limit');
+    }
+
+    if (mensaFilter && (mensaFilter != 'Show All')) {
+      result = result && mensaFilter === self.data('mensa');
     }
 
 
-	var membersFilter, topicsFilter, topicsFilterArr, mensaFilter, dateFilter, myFilter;
+    if (dateFilter && (dateFilter != '')) {
+    var dateTime = self.data('date-time');
+      result = result && (self.data('date-time').indexOf(dateFilter)!==-1);
+    }
 
-	function updateFilters() {
-	  $('.task-list-row').hide().filter(function() {
-		var
-		  self = $(this),
-		  result = true;
+    if (self.data('topics') == '' && topicsFilterArr!='') {
+    result = false;
+    }
 
-		if (membersFilter && (membersFilter != 'Show All')) {
-		  result = result && membersFilter === self.data('members-limit');
-		}
+    if (topicsFilter && topicsFilterArr!='') {
 
-		if (mensaFilter && (mensaFilter != 'Show All')) {
-		  result = result && mensaFilter === self.data('mensa');
-		}
+        var tr=true;
+        var i;
+        for (i=0;i<topicsFilterArr.length;i++) {
 
+            if (self.data('topics').indexOf(topicsFilterArr[i]) == -1) {
+                tr = false;
+                break;
+            }
+        }
+            result = result && tr;
+    }
 
-		if (dateFilter && (dateFilter != '')) {
-		var dateTime = self.data('date-time');
-		  result = result && (self.data('date-time').indexOf(dateFilter)!==-1);
-		}
+    if (fullFilter==true) {
+        alert(self.data('members').length);
+        alert(self.data("members-limit"));
+        result = result && (self.data('members').length===self.data("members-limit"));
+    }
 
-		if (self.data('topics') == '' && topicsFilterArr!='') {
-		result = false;
-		}
+    return result;
+  }).show();
 
-		if (topicsFilter && topicsFilterArr!='') {
+}
 
-			var tr=true;
-			var i;
-			for (i=0;i<topicsFilterArr.length;i++) {
+function getValue( elem ) {
+    var a ="";
+    elem.each(function( index ) {
+        let b = $( this ).text();
+        a = a.concat(b);
+        a = a.concat(",");
+    });
+    a = a.slice(0,-1);
+    return a;
+}
 
-				if (self.data('topics').indexOf(topicsFilterArr[i]) == -1) {
-					tr = false;
-					break;
-				}
-			}
-				result = result && tr;
-		}
+$('#topics-filter').on('change', function() {
+    topicsFilter = getValue($("div.dropdown-menu.show div.inner ul li.selected"));
+    topicsFilterArr = topicsFilter.split(",");
+    updateFilters();
+    });
 
+$('#members-limit-filter').on('change', function() {
+    membersFilter = $('select[id="members-limit-filter"] :selected').attr('data-filter-value');
+    updateFilters();
+});
 
+$('#mensa-filter').on('change', function() {
+    mensaFilter = $('select[id="mensa-filter"] :selected').val();
+    updateFilters();
+});
 
-		return result;
-	  }).show();
+$('#date-filter').on('change', function() {
+    dateFilter=$('#date-filter').val();
+    updateFilters();
 
-	}
+});
 
-	function getValue( elem ) {
-		var a ="";
-		elem.each(function( index ) {
-			let b = $( this ).text();
-	    	a = a.concat(b);
-	    	a = a.concat(",");
-		});
-		a = a.slice(0,-1);
-		return a;
-	}
+$('.clear-date').on('click', function() {
+    $('.date').datepicker('setDate', null);
+    dateFilter='';
+    updateFilters();
+});
 
-	$('#topics-filter').on('change', function() {
-		topicsFilter = getValue($("div.dropdown-menu.show div.inner ul li.selected"));
-		topicsFilterArr = topicsFilter.split(",");
-		updateFilters();
-		});
-
-	$('#members-limit-filter').on('change', function() {
-	  membersFilter = $('select[id="members-limit-filter"] :selected').attr('data-filter-value');
-	  updateFilters();
-	});
-
-	$('#mensa-filter').on('change', function() {
-	  mensaFilter = $('select[id="mensa-filter"] :selected').val();
-	  updateFilters();
-	});
-
-	$('#date-filter').on('change', function() {
-
-	  dateFilter=$('#date-filter').val();
-	  updateFilters();
-	});
-
-
-	$( "#show-my-meetups" ).on( "click", function() {
-		if ($( "#show-my-meetups" ).attr("class")=="checked") {
-			myFilter=true;
-			$("#show-my-meetups").removeClass("checked");
-			$("#show-my-meetups").addClass("not");
-			}
-		else {
-			myFilter=false;
-			$("#show-my-meetups").removeClass("not");
-			$("#show-my-meetups").addClass("checked");
-			}
-
-		updateFilters();
-	});
