2845016f837d53d80fb99f76285d9585cc117d37
Milan Szoszkiewicz <m.szoszk@gmail.com>
2019-06-27 17:32:38 +0200

    Hotfix where registration was broken

--- users/urls.py
+++ users/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('meetup/<int:pk>/delete/', MeetupDeleteView.as_view(), name='meetup-delete'),
     path('meetup/<int:pk>/leave', views.leaveMeetup, name='leave_meetup'),
     path('meetup/<int:pk>/join', views.joinMeetup, name='join_meetup'),
+    path('user/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
