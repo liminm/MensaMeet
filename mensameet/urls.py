6693c3bccbf3ab4f70822c9cdbe451c621558928
Limin <limin.malek@campus.tu-berlin.de>
2023-01-17 10:54:13 +0100

    Added guest view

--- mensameet/urls.py
+++ mensameet/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from mensameet.views import (
 MeetupListViewAll,
 MeetupListViewAllReverse,
+MeetupListViewAllGuest,
+MeetupListViewAllGuestReverse,
 )
 
 urlpatterns = [
     path('', MeetupListViewAll.as_view(), name='mensameet-home'),
     path('reverse', MeetupListViewAllReverse.as_view(), name='mensameet-home-reverse'),
-
+    path('guest', MeetupListViewAllGuest.as_view(), name='mensameet-home-guest'),
+    path('guest-reverse', MeetupListViewAllGuestReverse.as_view(), name='mensameet-home-guest-reverse'),  
 ]
--- mensameet/views.py
+++ mensameet/views.py
@@ -56,3 +56,43 @@ class MeetupListViewAllReverse(LoginRequiredMixin, ListView):
 
 	template_name = 'mensameet/home.html'	
 	context_object_name = 'meetupsAll'
+
+
+class MeetupListViewAllGuest(ListView):
+	model = Meetup
+
+	def get_queryset(self):
+		qs = Meetup.objects.all().order_by('start_time')
+		return qs
+
+
+	def get_context_data(self, **kwargs):
+		context = super(MeetupListViewAllGuest, self).get_context_data(**kwargs)
+		context.update({'topicsAll': Topic.objects.all().order_by('title'),
+			'mensaAll': Mensa.objects.all().order_by('title'),
+        	})
+		context['b_normal_order'] = True
+
+		return context
+
+	template_name = 'mensameet/home_guest.html'	
+	context_object_name = 'meetupsAll'
+
+class MeetupListViewAllGuestReverse(ListView):
+	model = Meetup
+
+	def get_queryset(self):
+		qs = Meetup.objects.all().order_by('-start_time')
+		return qs
+
+	def get_context_data(self, **kwargs):
+		context = super(MeetupListViewAllGuestReverse, self).get_context_data(**kwargs)
+		context.update({'topicsAll': Topic.objects.all().order_by('title'),
+			'mensaAll': Mensa.objects.all().order_by('title'),
+			})
+		context['b_normal_order'] = True
+
+		return context
+
+	template_name = 'mensameet/home_guest.html'	
+	context_object_name = 'meetupsAll'
